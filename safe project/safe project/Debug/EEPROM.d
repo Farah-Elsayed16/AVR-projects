02b0e24848f56010103fb865cc561675305a775a
Farah-Elsayed16 <farahelsayed169@gmail.com>
2024-09-30 13:28:13 +0300

    Delete safe project directory

--- safe project/safe project/Debug/EEPROM.d
+++
@@ -1,5 +0,0 @@
-EEPROM.d EEPROM.o: .././EEPROM.c .././Register_names.h .././STD_MACROS.h
-
-.././Register_names.h:
-
-.././STD_MACROS.h:
--- safe project/safe project/Debug/keybad.d
+++
@@ -1,5 +0,0 @@
-keybad.d keybad.o: .././keybad.c .././DIO.h .././keybad.h
-
-.././DIO.h:
-
-.././keybad.h:
